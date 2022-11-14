a07cdd3b5b75121e384b06a6c9174fb924c14d56
Evan Wu <evan.wu@authme.com>
2022-11-14 16:17:37 +0800

    modify Package

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
                     name: "AuthMe",
                     // url: "https://github.com/evan3rd/AuthMe/AuthMe.xcframework.zip",
                     url: "https://github.com/evan3rd/AuthMe/blob/develop/AuthMe.xcframework.zip",
-                    checksum: "37f7e9608c6070fb3d1bd2e85f36ffcd145d34c98c368899725e463f54befecf"
+                    //checksum: "37f7e9608c6070fb3d1bd2e85f36ffcd145d34c98c368899725e463f54befecf"
+                    checksum: "a942872ecb3a978ee0b2369719eaa8a430e216f76c5a59fab69b299cca20dab9"
                 ),
         .testTarget(
             name: "MyLibraryTests",
