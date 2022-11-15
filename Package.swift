98ec6e77416394401cf293a552071e75cd7e88aa
Evan Wu <evan.wu@authme.com>
2022-11-15 11:29:39 +0800

    modify Package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyLibrary",
-            targets: ["MyLibrary", "AuthMe"]
+            targets: [
+                "MyLibrary",
+                //"AuthMe",
+                "FirebaseAnalytics"]
         ),
     ],
     dependencies: [
@@ -22,14 +25,19 @@ let package = Package(
         .target(
             name: "MyLibrary"
         ),
+//        .binaryTarget(
+//            name: "AuthMe",
+//            // url: "https://github.com/evan3rd/AuthMe/AuthMe.xcframework.zip",
+//            //url: "https://github.com/evan3rd/AuthMe/blob/develop/AuthMe.xcframework.zip",
+//            //url: "http://192.168.64.2/dashboard/AuthMe.xcframework.zip",
+//            url: "https://github.com/evan3rd/MyLibrary/tree/develop/Sources/AuthMe.xcframework.zip",
+//            //checksum: "37f7e9608c6070fb3d1bd2e85f36ffcd145d34c98c368899725e463f54befecf"
+//            checksum: "4505d2a3dc17bc64d603ec7ab476615f81d4b4f01a48f8f68e728cae4e8bda72"
+//        ),
         .binaryTarget(
-            name: "AuthMe",
-            // url: "https://github.com/evan3rd/AuthMe/AuthMe.xcframework.zip",
-            //url: "https://github.com/evan3rd/AuthMe/blob/develop/AuthMe.xcframework.zip",
-            //url: "http://192.168.64.2/dashboard/AuthMe.xcframework.zip",
-            url: "https://github.com/evan3rd/MyLibrary/tree/develop/Sources/AuthMe.xcframework.zip",
-            //checksum: "37f7e9608c6070fb3d1bd2e85f36ffcd145d34c98c368899725e463f54befecf"
-            checksum: "4505d2a3dc17bc64d603ec7ab476615f81d4b4f01a48f8f68e728cae4e8bda72"
+          name: "FirebaseAnalytics",
+          url: "https://dl.google.com/firebase/ios/swiftpm/10.1.0/FirebaseAnalytics.zip",
+          checksum: "c5429b2e293d7ab2ed2f291bd5edf13f7612b4b69c8261259f48a7b948fa824d"
         ),
     ]
 )
