015e5e1ae37e97418cf1c6e69c9f0a655b90eabb
Evan Wu <evan.wu@authme.com>
2022-11-15 18:00:23 +0800

    modify Package

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "MyLibrary",
             targets: [
                 "MyLibrary",
-                //"AuthMe",
+                "AuthMe",
                 "FirebaseAnalytics"]
         ),
     ],
