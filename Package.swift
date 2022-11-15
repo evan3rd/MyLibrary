5bcc40b0d1acfdec02b2f714c601f6f53ca8a7c1
Evan Wu <evan.wu@authme.com>
2022-11-15 10:58:28 +0800

    modify Package

--- Package.swift
+++ Package.swift
@@ -25,8 +25,9 @@ let package = Package(
         .binaryTarget(
             name: "AuthMe",
             // url: "https://github.com/evan3rd/AuthMe/AuthMe.xcframework.zip",
-            url: "https://github.com/evan3rd/AuthMe/blob/develop/AuthMe.xcframework.zip",
+            //url: "https://github.com/evan3rd/AuthMe/blob/develop/AuthMe.xcframework.zip",
             //url: "http://192.168.64.2/dashboard/AuthMe.xcframework.zip",
+            url: "https://github.com/evan3rd/MyLibrary/tree/develop/Sources/AuthMe.xcframework.zip",
             //checksum: "37f7e9608c6070fb3d1bd2e85f36ffcd145d34c98c368899725e463f54befecf"
             checksum: "4505d2a3dc17bc64d603ec7ab476615f81d4b4f01a48f8f68e728cae4e8bda72"
         ),
